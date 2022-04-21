996377fabc405b2e13b4896109c9f346c6248858
dumasti <51967779+dumasti@users.noreply.github.com>
2022-04-21 12:35:05 +0300

    Update revoke_ssl.sh
    add cert_base

--- revoke_ssl.sh
+++ revoke_ssl.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
 echo "Which CA to use? "
-echo `ls ~/ | grep easyrsa`
+echo `ls ~/ | grep -E -i 'CA|easy|rsa'`
 read -r
 ca=$REPLY
 cd ~/$ca/
-echo "For which server to revoke the certificate? "
+echo "For whom/for what to revoke the certificate? ((S)erver/(U)ser/(C)ancel) "
 echo `ls pki/issued/ | cut -d '.' -f 1`
 read -r
 name_crt=$REPLY
-printf "yes" | ./easyrsa revoke $name_crt
-./easyrsa gen-crl
-cp pki/crl.pem ~/ssl_scripts/ssl_certs/
+if [ -e ~/ssl_scripts/ssl_certs/revoke ]; then
+        printf "yes" | ./easyrsa revoke $name_crt
+        ./easyrsa gen-crl
+        cp pki/crl.pem ~/ssl_scripts/ssl_certs/revoke/
+else
+        mkdir ~/ssl_scripts/ssl_certs/revoke
+        printf "yes" | ./easyrsa revoke $name_crt
+        ./easyrsa gen-crl
+        cp pki/crl.pem ~/ssl_scripts/ssl_certs/revoke/
+fi
 rm ~/$ca/pki/issued/$name_crt.crt
+rm ~/$ca/pki/private/$name_crt.*
+rm -r ~/ssl_scripts/ssl_certs/$name_crt
+echo "$name_crt `date | cut -d " " -f2,3,4` $ca REVOKE" >> ~/ssl_scripts/cert_base
 echo "DONE!"

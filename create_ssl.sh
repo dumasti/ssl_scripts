47e18b05f26d0cb2a0d5de9e79f4b69ef9adee90
dumasti <51967779+dumasti@users.noreply.github.com>
2022-04-21 11:53:42 +0300

    Update create_ssl.sh
    Del openssl options
    Cheng grep method
    Cheng create cert

--- create_ssl.sh
+++ create_ssl.sh
@@ -1,43 +1,32 @@
 #!/bin/bash
 
-echo "How is name your site? "
-read -r
-site=$REPLY
-name=`echo $site | cut -d '.' -f 1`
-sudo openssl genrsa -out $site.key 4096
-echo "C=US
-ST=California
-L=San_Francisco
-O=Copyleft_Certificate_Co
-OU=My_Organizational_Unit
-CN=$site"
-echo "Correct data or other? (Y/n/(C)acel)"
+echo "For whom/for what the certificate? ((S)erver/(U)ser/(C)ancel) "
 while true; do
         read -r
-        data=$REPLY
-        if [[ "$data" == "Y" ]] || [[ "$data" ==  "y" ]]; then
-                sudo openssl req -new -key $site.key -sha256 -out $name.req -nodes -subj "/C=US/ST=California/L=San_Francisco/O=Copyleft_Certificate_Co/OU=My_Organizational_Unit/CN=$site"
+        object=$REPLY
+        if [[ "$object" == "S" ]] || [[ "$object" ==  "s" ]]; then
+                echo "What is the name of your site? "
+                read -r
+                name=$REPLY
                 break
-        elif [[ "$data" == "N" ]] || [[ "$data" == "n" ]]; then
-                sudo openssl req -new -key $site.key -sha256 -out $name.req -nodes
+        elif [[ "$object" == "U" ]] || [[ "$object" ==  "u" ]]; then
+                echo "What is the name of your user? "
+                read -r
+                name=$REPLY
                 break
-        elif [[ "$data" == "C" ]] || [[ "$data" == "c" ]]; then
+        elif [[ "$object" == "C" ]] || [[ "$object" == "c" ]]; then
                 exit
         else
-                echo "Correct answer is Y/y/N/n/C/c only!
-Correct data or other? (Y/n/(C)ancel"
+                echo "Correct answer is S/s/U/u/C/c only!
+                Correct data or other? ((S)erver/(U)ser/(C)ancel"
         fi
 done
-sudo mv *.{key,req} ssl_certs/
 echo "Which CA to use? "
-echo `ls ~/ | grep easyrsa`
+echo `ls ~/ | grep -E -i 'CA|easy|rsa'`
 read -r
 ca=$REPLY
 cd ~/$ca/
-./easyrsa gen-dh
-mv pki/dh.pem ~/ssl_scripts/ssl_certs/
-./easyrsa import-req ~/ssl_scripts/ssl_certs/$name.req $name
-echo "For how long to sign the certificate?
+echo "How long to sign the certificate (in days)?
 365 - 1 year
 730 - 2 years
 1095 - 3 years
@@ -45,12 +34,35 @@ echo "For how long to sign the certificate?
 1825 - 5 years "
 read -r
 year_new=$REPLY
-year_old=`cat ~/$ca/vars | grep "set_var EASYRSA_CERT_EXPIRE" | cut -d ' ' -f 2`
-sed -i "s/set_var $year_old/set_var EASYRSA_CERT_EXPIRE $year_new/" ~/$ca/vars
-printf "yes" | ./easyrsa sign-req server $name
-cp ~/$ca/pki/issued/$name.crt ~/ssl_scripts/ssl_certs/
-sudo rm ~/ssl_scripts/ssl_certs/$name.req
-cat ~/$ca/pki/issued/$name.crt ~/$ca/pki/SubCA.crt > ~/ssl_scripts/ssl_certs/$name.fullchain.crt
+mkdir ~/ssl_scripts/ssl_certs/$name
+year_old=`cat ~/$ca/vars | grep "set_var EASYRSA_CERT_EXPIRE"`
+sed -i "s/$year_old/set_var EASYRSA_CERT_EXPIRE $year_new/" ~/$ca/vars
+./easyrsa gen-req $name nopass
+if [[ "$object" == "S" ]] || [[ "$object" ==  "s" ]]; then
+        ./easyrsa sign-req server $name
+        echo "Do you need dh.pem? (y/n)"
+        read -r
+        dh=$REPLY
+        if [[ "$dh" == "y" ]]; then
+                if [ -e pki/dh.pem ]; then
+                        mv pki/dh.pem ~/ssl_scripts/ssl_certs/$name
+                else
+                        ./easyrsa gen-dh
+                        mv pki/dh.pem ~/ssl_scripts/ssl_certs/$name
+                fi
+        fi
+        cp ~/$ca/pki/issued/$name.crt ~/ssl_scripts/ssl_certs/$name
+        cp ~/$ca/pki/RootCA.crt ~/ssl_scripts/ssl_certs/$name
+        cp ~/$ca/pki/private/$name.key ~/ssl_scripts/ssl_certs/$name
+        cat ~/$ca/pki/issued/$name.crt ~/$ca/pki/SubCA.crt > ~/ssl_scripts/ssl_certs/$name/$name.fullchain.crt
+elif [[ "$object" == "U" ]] || [[ "$object" ==  "u" ]]; then
+        ./easyrsa sign-req client $name
+        ./easyrsa export-p12 $name
+        cp ~/$ca/pki/issued/$name.crt ~/ssl_scripts/ssl_certs/$name
+        cp ~/$ca/pki/RootCA.crt ~/ssl_scripts/ssl_certs/$name
+        cp ~/$ca/pki/private/$name.{key,p12} ~/ssl_scripts/ssl_certs/$name
+fi
 whoami=`whoami`
-sudo chown -R $whoami:$whoami ~/ssl_scripts/ssl_certs
+sudo chown -R $whoami:$whoami ~/ssl_scripts/ssl_certs/$name/
 echo "DONE!"
+
